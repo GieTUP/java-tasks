c11da1ff27757dc5b04fb94f1f533bc6ffdfaf0f
GieTUP <evangelista.agdiaz@gmail.com>
2024-09-10 17:37:12 +0800

    main

---
+++ GuessTheNumber.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+
+        
+        System.out.println("1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31");
+        System.out.print("Is your number here? (1 for yes, 0 for no): ");
+        int response = input.nextInt();
+        if (response == 1) {
+            sum += 1;
+        }
+
+       
+        System.out.println("2, 3, 6, 7, 10, 11, 14, 15, 18, 19, 22, 23, 26, 27, 30, 31");
+        System.out.print("Is your number here? (1 for yes, 0 for no): ");
+        response = input.nextInt();
+        if (response == 1) {
+            sum += 2;
+        }
+
+        
+        System.out.println("4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31");
+        System.out.print("Is your number here? (1 for yes, 0 for no): ");
+        response = input.nextInt();
+        if (response == 1) {
+            sum += 4;
+        }
+
+        
+        System.out.println("8, 9, 10, 11, 12, 13, 14, 15, 24, 25, 26, 27, 28, 29, 30, 31");
+        System.out.print("Is your number here? (1 for yes, 0 for no): ");
+        response = input.nextInt();
+        if (response == 1) {
+            sum += 8;
+        }
+
+      
+        System.out.println("16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31");
+        System.out.print("Is your number here? (1 for yes, 0 for no): ");
+        response = input.nextInt();
+        if (response == 1) {
+            sum += 16;
+        }
+
+        
+        System.out.println("Your number is: " + sum);
+        input.close();
+    }
+}
