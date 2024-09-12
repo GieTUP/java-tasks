e42f1c1954ee2a25ade16f031adce813ecd466fe
GieTUP <evangelista.agdiaz@gmail.com>
2024-09-13 01:36:17 +0800

    sleepy

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main{
+
+char yn = 'y';
+//CONTINUE
+    cout<<"\nDo you wish to continue?<y/n>";
+    cin>>yn;
+
+
+    while(yn!='y' && yn!='n'){
+        cout<<"Please enter 'y' or 'n' only:";
+        cin>>yn;
+    }
+    if(yn=='y'){
+        main();
+    }
+
+
+
+    return 0;
+}
