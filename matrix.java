07ba7f9f2b141f9c7aa9a494351d6e864bf2761e
GieTUP <evangelista.agdiaz@gmail.com>
2024-09-19 22:43:49 +0800

    g next week

--- matrix.java
+++ matrix.java
@@ -1,10 +1,23 @@
 public class matrix {
     public static void main(String[] args) {
-        int[][] myNumbers = { { 1, 2, 3, 4 }, { 5, 6, 7 } };
-        for (int i = 0; i < myNumbers.length; ++i) {
-            for (int j = 0; j < myNumbers[i].length; ++j) {
-                System.out.print(myNumbers[i][j]);
-            }
-        }
+       /*  int[][] myNumbers = { { 1, 2, 3, 4 }, { 5, 6, 7 } };
+       for (int i = 0; i < myNumbers.length; ++i) {
+          for (int j = 0; j < myNumbers[i].length; ++j) {
+              System.out.print(myNumbers[i][j]);
+          }
+       } */
+       
+    /*   int[][] myNumbers = { { 1, 2, 3, 4 }, { 5, 6, 7 } };
+       myNumbers[1][2] = 9;
+       System.out.println(myNumbers[1][2]);
+       */
+      
+    String[] Sections = { "S09", "S05", "S04" };
+    for (String i : Sections) {
+        System.out.println(i);
+    }
+
+
     }
 }
+
